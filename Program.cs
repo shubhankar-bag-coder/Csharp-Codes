5fd2af4ca958b3f76ed586522faa681204cb2296
shubhankar-bag-coder <bagshubhankar@gmail.com>
2022-08-05 16:28:44 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Default_App
+{
+
+ class Program
+ {
+   static void Main(string[] args)
+    {
+         Console.WriteLine("C# is used for:" +
+             "\n  Mobile applications" +
+             "\n  Desktop applications " +
+             "\n  Web applications " +
+             "\n  Web services " +
+             "\n  Websites " +
+             "\n  Games" +
+             "\n  VR" +
+             "\n  Database applications" +
+             "\n  And much, much more!");
+
+    }
+ }
+}
